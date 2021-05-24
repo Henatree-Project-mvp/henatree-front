ccb02fe2d399ce03a0afaf6a56be6d7d52507f95
Aurélie PREAUD <aureliepreaud@MacBook-Air-de-Aurelie.local>
2021-05-24 08:50:32 +0200

    modifs json et outingScreen

--- containers/OutingsScreen.js
+++ containers/OutingsScreen.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { useRoute } from "@react-navigation/core";
 import { useNavigation } from "@react-navigation/core";
-import { Text, View, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Image,
+} from "react-native";
+
+//Import des couleurs
 import colors from "../assets/colors";
+const { yellow, blue, darkBlue, dark, errorColor, greyButton, greyFont } =
+  colors;
+//Import des datas
 import categories from "../assets/categories.json";
 import sorties from "../assets/sorties.json";
 
@@ -11,13 +23,38 @@ export default function OutingsScreen({ navigation }) {
   return (
     <View>
       <Text style={{ fontSize: 26 }}>Sorties à la une</Text>
-      <TouchableOpacity
+      <FlatList
+        data={sorties}
+        keyExtractor={(item) => item.categorie}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity>
+              <Image
+                style={styles.outingPicture}
+                source={{ uri: item.photo }}
+              />
+              <Text style={styles.outingTitle}>{item.titre}</Text>
+              <Text
+                style={styles.txtInfos}
+              >{`${item.dateSortie} ${item.horaireSortie} - ${item.nbParticipants}/${item.maxParticipants} participants - ${item.ville} `}</Text>
+            </TouchableOpacity>
+          );
+        }}
+      />
+      {/* <TouchableOpacity
         onPress={() => {
           navigation.navigate("OutingDetail");
         }}
       >
         <Text style={{ fontSize: 18, color: "blue" }}>Voir une sortie</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  outingPicture: {
+    width: "100%",
+    height: 250,
+  },
+});
